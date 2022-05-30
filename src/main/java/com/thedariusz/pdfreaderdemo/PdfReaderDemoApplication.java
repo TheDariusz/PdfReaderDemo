9ce1ed73d0b98d6a64ab8305f79b242861c605bc
Dariusz Szczepański <thedariusz@gmail.com>
2022-05-30 22:04:05 +0200

    initial configuration:

---
+++ src/main/java/com/thedariusz/pdfreaderdemo/PdfReaderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thedariusz.pdfreaderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfReaderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfReaderDemoApplication.class, args);
+    }
+}
+
---
+++ src/test/java/com/thedariusz/pdfreaderdemo/PdfReaderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedariusz.pdfreaderdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfReaderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
