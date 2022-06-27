b95b5402cbc95d914902d95b95c5aff65981a955
Dariusz Szczepański <thedariusz@gmail.com>
2022-06-27 21:28:14 +0200

    final version of ImgwAlertMapper with tests

--- src/main/java/com/thedariusz/pdfreaderdemo/TextPatterns.java
+++ src/main/java/com/thedariusz/pdfreaderdemo/TextPatterns.java
@@ -8,8 +8,12 @@ public class TextPatterns {
     public static final String CAPITAL_LETTER_SMALL_LETTERS_SLASH = "([A-Z][a-ząćęłńóśóżź\\s]+)\\/";
     public static final String SLASH_DIGIT_NEWLINE = "\\/(\\d)[ |\\n]";
     public static final String SLASH_DIGIT_SPACE_CAPITAL_LETTERS = "\\/\\d\\s([A-Z]+)";
-    public static final String ANY_LETTERS_AND_DIGITS_IN_BRACKETS = "([A-Za-ząćęłńóśóżź]+)\\((\\d+)\\)";
+    public static final String ANY_LETTERS_AND_DIGITS_IN_BRACKETS = "([A-ZĄĆĘŁŃÓŚÓŻŹa-ząćęłńóśóżź][A-ZĄĆĘŁŃÓŚÓŻŹa-ząćęłńóśóżź \\-]+)\\((\\d+)\\)";
     public static final String PRAWDOPODOBIENSTWO_WITH_DIGITS_AND_PERCENTAGE = "Prawdopodobieństwo (\\d+)%";
     public static final String PRZEBIEG_ANY_TEXT_ENDED_WITH_SMS = "Przebieg ([\\S\\s\\n]+)SMS";
     public static final String SMS_ANY_TEXT_ENDED_WITH_RSO = "SMS ([\\S\\s\\n]+)RSO";
+
+    private TextPatterns() {
+        throw new IllegalStateException("Utility class");
+    }
 }
--- src/test/java/com/thedariusz/pdfreaderdemo/ImgwAlertMapperTest.java
+++ src/test/java/com/thedariusz/pdfreaderdemo/ImgwAlertMapperTest.java
@@ -26,25 +26,36 @@ class ImgwAlertMapperTest {
             temp. maks 34 st, temp min 18 st. Dotyczy powiatów: milicki i oleśnicki.
             RSO Woj. dolnośląskie (2 powiaty), IMGW-PIB wydał ostrzeżenie drugiego stopnia o upałach
             Uwagi Brak.
-            Zjawisko/Stopień zagrożenia Upał/2 ZMIANA
+            Zjawisko/Stopień zagrożenia Upał/2
             Obszar (w nawiasie numer
             ostrzeżenia dla powiatu)
-            powiaty: oławski(36), strzeliński(36), ząbkowicki(46)
-            Ważność od godz. 12:36 dnia 18.06.2022 do godz. 20:00 dnia 19.06.2022
-            Prawdopodobieństwo 80%
+            powiaty: będziński(54), bieruńsko-lędziński(49), Bytom(50), Chorzów(49), Częstochowa(54),
+            częstochowski(54), Dąbrowa Górnicza(54), Gliwice(50), gliwicki(51), Jastrzębie-Zdrój(50),
+            Jaworzno(51), Katowice(49), kłobucki(53), lubliniecki(53), mikołowski(49), Mysłowice(50),
+            myszkowski(53), Piekary Śląskie(51), pszczyński(50), raciborski(49), Ruda Śląska(49),
+            rybnicki(49), Rybnik(49), Siemianowice Śląskie(50), Sosnowiec(52), Świętochłowice(49),
+            tarnogórski(53), Tychy(49), wodzisławski(50), Zabrze(49), zawierciański(54), Żory(49)
             Instytut Meteorologii i Gospodarki Wodnej - Państwowy Instytut Badawczy
-            Centralne Biuro Prognoz Meteorologicznych - Wydział we Wrocławiu
-            51-616 Wrocław ul. Parkowa 30
-            tel: 071-320-01-50, fax: 071-348-73-37
-            email: meteo.wroclaw@imgw.pl
+            Biuro Prognoz Meteorologicznych w Krakowie
+            30-215 Kraków ul. Piotra Borowego 14
+            tel: 12-6398150, fax: 12-4251973
+            email: meteo.krakow@imgw.pl
             www: www.imgw.pl
-            strona 1 z 3
-            Przebieg Prognozuje się upały. Temperatura maksymalna w dzień od 30°C do 33°C, lokalnie w niedzielę
-            do 34°C. Temperatura minimalna w nocy od 15°C do 18°C.
-            SMS IMGW-PIB OSTRZEGA: UPAŁ/2 dolnośląskie (3 powiaty) od 12:36/18.06 do 20:00/19.06.2022
-            temp. maks 34 st, temp min 18 st. Dotyczy powiatów: oławski, strzeliński i ząbkowicki.
-            RSO Woj. dolnośląskie (3 powiaty), IMGW-PIB wydał ostrzeżenie drugiego stopnia o upałach
-            Uwagi Brak.
+            strona 1 z 2
+            Ważność od godz. 12:00 dnia 25.06.2022 do godz. 20:00 dnia 28.06.2022
+            Prawdopodobieństwo 80%
+            Przebieg Prognozuje się upał. Temperatura maksymalna w dzień od 29°C do 32°C, w poniedziałek (27.06)
+            i wtorek (28.06) miejscami do 34°C. Temperatura minimalna w nocy 25/26.06 od 13°C do 17°C,
+            a w kolejne od 16°C do 20°C.
+            SMS IMGW-PIB OSTRZEGA: UPAŁ/2 śląskie (32 powiatów) od 12:00/25.06 do 20:00/28.06.2022
+            temp. maks 29-32st., lok.34st, temp min 17-20st.,lok.13-17st. Dotyczy powiatów: będziński,
+            bieruńsko-lędziński, Bytom, Chorzów, Częstochowa, częstochowski, Dąbrowa Górnicza,
+            Gliwice, gliwicki, Jastrzębie-Zdrój, Jaworzno, Katowice, kłobucki, lubliniecki, mikołowski,
+            Mysłowice, myszkowski, Piekary Śląskie, pszczyński, raciborski, Ruda Śląska, rybnicki, Rybnik,
+            Siemianowice Śląskie, Sosnowiec, Świętochłowice, tarnogórski, Tychy, wodzisławski, Zabrze,
+            zawierciański i Żory.
+            RSO Woj. śląskie (32 powiatów), IMGW-PIB wydał ostrzeżenie drugiego stopnia o upałach
+            Uwagi Ostrzeżenie może być kontynuowane.
             Zjawisko/Stopień zagrożenia Upał/1
             Obszar (w nawiasie numer
             ostrzeżenia dla powiatu)
@@ -96,12 +107,45 @@ class ImgwAlertMapperTest {
             strona 3 z 3
             """;
 
+    private final String sampleAlert2 = """
+            Upał/2
+            Obszar (w nawiasie numer
+            ostrzeżenia dla powiatu)
+            powiaty: będziński(54), bieruńsko-lędziński(49), Bytom(50), Chorzów(49), Częstochowa(54),
+            częstochowski(54), Dąbrowa Górnicza(54), Gliwice(50), gliwicki(51), Jastrzębie-Zdrój(50),
+            Jaworzno(51), Katowice(49), kłobucki(53), lubliniecki(53), mikołowski(49), Mysłowice(50),
+            myszkowski(53), Piekary Śląskie(51), pszczyński(50), raciborski(49), Ruda Śląska(49),
+            rybnicki(49), Rybnik(49), Siemianowice Śląskie(50), Sosnowiec(52), Świętochłowice(49),
+            tarnogórski(53), Tychy(49), wodzisławski(50), Zabrze(49), zawierciański(54), Żory(49)
+            Instytut Meteorologii i Gospodarki Wodnej - Państwowy Instytut Badawczy
+            Biuro Prognoz Meteorologicznych w Krakowie
+            30-215 Kraków ul. Piotra Borowego 14
+            tel: 12-6398150, fax: 12-4251973
+            email: meteo.krakow@imgw.pl
+            www: www.imgw.pl
+            strona 1 z 2
+            Ważność od godz. 12:00 dnia 25.06.2022 do godz. 20:00 dnia 28.06.2022
+            Prawdopodobieństwo 80%
+            Przebieg Prognozuje się upał. Temperatura maksymalna w dzień od 29°C do 32°C, w poniedziałek (27.06)
+            i wtorek (28.06) miejscami do 34°C. Temperatura minimalna w nocy 25/26.06 od 13°C do 17°C,
+            a w kolejne od 16°C do 20°C.
+            SMS IMGW-PIB OSTRZEGA: UPAŁ/2 śląskie (32 powiatów) od 12:00/25.06 do 20:00/28.06.2022
+            temp. maks 29-32st., lok.34st, temp min 17-20st.,lok.13-17st. Dotyczy powiatów: będziński,
+            bieruńsko-lędziński, Bytom, Chorzów, Częstochowa, częstochowski, Dąbrowa Górnicza,
+            Gliwice, gliwicki, Jastrzębie-Zdrój, Jaworzno, Katowice, kłobucki, lubliniecki, mikołowski,
+            Mysłowice, myszkowski, Piekary Śląskie, pszczyński, raciborski, Ruda Śląska, rybnicki, Rybnik,
+            Siemianowice Śląskie, Sosnowiec, Świętochłowice, tarnogórski, Tychy, wodzisławski, Zabrze,
+            zawierciański i Żory.
+            RSO Woj. śląskie (32 powiatów), IMGW-PIB wydał ostrzeżenie drugiego stopnia o upałach
+            Uwagi Ostrzeżenie może być kontynuowane.
+            """;
+
     @Test
     void checkingModelMapping() {
         ImgwAlertMapper imgwAlertMapper = new ImgwAlertMapper();
         ImgwMeteoWarning imgwMeteoWarning = imgwAlertMapper.toModel(sampleAlert);
         assertFalse(imgwMeteoWarning.localMeteoWarnings().isEmpty());
-        assertTrue(imgwMeteoWarning.number()>0);
+        assertTrue(imgwMeteoWarning.number() > 0);
         assertEquals(Voivodeship.DS, imgwMeteoWarning.voivodeship());
         assertEquals("2022-06-18T12:42", imgwMeteoWarning.published().toString());
 
