46c6d0f78ae009e50caf9beccccbf887a1e11d32
Dariusz Szczepański <thedariusz@gmail.com>
2022-07-15 22:30:34 +0200

    sql creation tables done

--- src/main/resources/db/migration/V1_1_0__CreateTables.sql
+++ src/main/resources/db/migration/V1_1_0__CreateTables.sql
@@ -24,16 +24,7 @@ CREATE TABLE meteo_alert
     alert_number   INTEGER   NOT NULL,
     published_at   TIMESTAMP NOT NULL,
 
-    CONSTRAINT fk_meteo_alert_voivodeship_id FOREIGN KEY (voivodeship_id) REFERENCES voivodeship (id) ON DELETE CASCADE
-);
-
-CREATE TABLE local_alert_county
-(
-    local_meteo_alert_id BIGINT NOT NULL,
-    county_id            BIGINT NOT NULL,
-
-    CONSTRAINT fk_local_alert_county_local_meteo_alert_id FOREIGN KEY (local_meteo_alert_id) REFERENCES local_meteo_alert (id) ON DELETE CASCADE,
-    CONSTRAINT fk_local_alert_county_county_id FOREIGN KEY (county_id) REFERENCES county (id) ON DELETE CASCADE
+    CONSTRAINT fk_meteo_alert_voivodeship_id FOREIGN KEY (voivodeship_id) REFERENCES voivodeship (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE local_meteo_alert
@@ -42,7 +33,6 @@ CREATE TABLE local_meteo_alert
     meteo_alert_id  BIGINT    NOT NULL,
     alert_type_id   BIGINT    NOT NULL,
     alert_status_id BIGINT    NOT NULL,
-    county_id       BIGINT    NOT NULL,
     start_date      TIMESTAMP NOT NULL,
     stop_date       TIMESTAMP,
     description     TEXT,
@@ -50,21 +40,32 @@ CREATE TABLE local_meteo_alert
     sms             TEXT,
 
     CONSTRAINT fk_local_meteo_alert_meteo_alert_id FOREIGN KEY (meteo_alert_id) REFERENCES meteo_alert (id) ON DELETE CASCADE,
-    CONSTRAINT fk_local_meteo_alert_alert_type_id FOREIGN KEY (alert_type_id) REFERENCES alert_type (id) ON DELETE CASCADE,
-    CONSTRAINT fk_local_meteo_alert_alert_status_id FOREIGN KEY (alert_status_id) REFERENCES alert_status (id) ON DELETE CASCADE,
-    CONSTRAINT fk_local_meteo_alert_county_id FOREIGN KEY (county_id) REFERENCES local_alert_county (county_id) ON DELETE CASCADE
+    CONSTRAINT fk_local_meteo_alert_alert_type_id FOREIGN KEY (alert_type_id) REFERENCES alert_type (id) ON UPDATE CASCADE,
+    CONSTRAINT fk_local_meteo_alert_alert_status_id FOREIGN KEY (alert_status_id) REFERENCES alert_status (id) ON UPDATE CASCADE
 );
-
 
 CREATE TABLE county
 (
     id                   BIGSERIAL    NOT NULL PRIMARY KEY,
-    local_meteo_alert_id BIGINT       NOT NULL,
     voivodeship_id       BIGINT       NOT NULL,
     name                 VARCHAR(255) NOT NULL,
     teryt_id             VARCHAR(255) NOT NULL,
 
-    CONSTRAINT fk_county_local_meteo_alert_id FOREIGN KEY (local_meteo_alert_id) REFERENCES local_alert_county (local_meteo_alert_id) ON DELETE CASCADE
+    CONSTRAINT fk_county_voivodeship_id FOREIGN KEY (voivodeship_id) REFERENCES voivodeship (id) ON UPDATE CASCADE
+
 );
+
+CREATE TABLE local_alert_county
+(
+    local_meteo_alert_id BIGINT NOT NULL,
+    county_id            BIGINT NOT NULL,
+
+    CONSTRAINT fk_local_alert_county_local_meteo_alert_id FOREIGN KEY (local_meteo_alert_id) REFERENCES local_meteo_alert (id) ON DELETE CASCADE,
+    CONSTRAINT fk_local_alert_county_county_id FOREIGN KEY (county_id) REFERENCES county (id) ON UPDATE CASCADE,
+    CONSTRAINT local_alert_county_unique UNIQUE (local_meteo_alert_id, county_id)
+);
+
+
+
 
 
