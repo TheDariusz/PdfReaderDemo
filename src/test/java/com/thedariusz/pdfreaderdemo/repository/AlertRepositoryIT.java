1edc8e86f020557b3b0b16d188f560c70e1e8894
Adam Masnik <adam.masnik@evry.com>
2022-07-27 00:43:22 +0200

    Testcontainers bootstrap

--- src/test/java/com/thedariusz/pdfreaderdemo/repository/AlertRepositoryIT.java
+++ src/test/java/com/thedariusz/pdfreaderdemo/repository/AlertRepositoryIT.java
@@ -1,34 +1,23 @@
 package com.thedariusz.pdfreaderdemo.repository;
 
 import com.thedariusz.pdfreaderdemo.repository.entity.AlertEntity;
-import com.thedariusz.pdfreaderdemo.repository.entity.AlertStatusEntity;
-import com.thedariusz.pdfreaderdemo.repository.entity.AlertTypeEntity;
 import com.thedariusz.pdfreaderdemo.repository.entity.VoivodeshipEntity;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-@ActiveProfiles("test-containers-flyway")
-@Testcontainers
-class AlertRepositoryTest {
+@ActiveProfiles("test")
+class AlertRepositoryIT {
 
     @Autowired
     private AlertRepository alertRepository;
 
     @Test
-    void shouldSaveMeteoAlertToTheBase() {
-
-
+    public void shouldSaveMeteoAlertToTheBase() {
         VoivodeshipEntity voivodeship = new VoivodeshipEntity();
         voivodeship.setCode("MZW");
         voivodeship.setName("Masovia");
