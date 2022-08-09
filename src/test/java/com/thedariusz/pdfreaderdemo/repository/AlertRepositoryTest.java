537fb63cb267d07cb308039ee8769c90344c8ca4
Dariusz Szczepański <thedariusz@gmail.com>
2022-08-09 21:13:16 +0200

    small changes in test configuration file

--- src/test/java/com/thedariusz/pdfreaderdemo/repository/AlertRepositoryTest.java
+++ src/test/java/com/thedariusz/pdfreaderdemo/repository/AlertRepositoryTest.java
@@ -5,11 +5,15 @@ import com.thedariusz.pdfreaderdemo.repository.entity.VoivodeshipEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
+@ActiveProfiles("test")
 @Testcontainers
 class AlertRepositoryTest {
 
@@ -26,7 +30,8 @@ class AlertRepositoryTest {
 
         AlertEntity alertEntity = new AlertEntity(123, LocalDateTime.now(), voivodeshipEntity);
 
+        AlertEntity alert = alertRepository.save(alertEntity);
 
-        alertRepository.save(alertEntity);
+        assertNotNull(alert);
     }
 }
